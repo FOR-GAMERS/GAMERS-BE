ed904f7b19ff99f8cb40e5ad4e9f56de3802f236
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2026-01-07 03:51:11 +0900

    Feat: Update DB Migrations for modifing directory (#11)

--- db/migrations/000004_modify_user_unique_constraint.up.sql
+++ db/migrations/000004_modify_user_unique_constraint.up.sql
@@ -1,5 +1,2 @@
--- Remove the unique index on tag column
-DROP INDEX IF EXISTS idx_users_tag ON users;
-
 -- Create composite unique index on username and tag
 CREATE UNIQUE INDEX idx_username_tag ON users(username, tag);

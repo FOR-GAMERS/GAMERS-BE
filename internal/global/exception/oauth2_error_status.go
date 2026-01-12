90b87f25389ce510c67b3bed4d7aa4b15b0ca7f9
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2026-01-12 19:41:08 +0900

    Feat: OAuth2 error status added (#9)

--- internal/global/exception/oauth2_error_status.go
+++ internal/global/exception/oauth2_error_status.go
@@ -6,4 +6,5 @@ var (
 	ErrDiscordCannotGetUserInfo = NewBusinessError(http.StatusUnauthorized, "failed to get user info from Discord", "OA001")
 	ErrDiscordUserCannotFound   = NewBusinessError(http.StatusNotFound, "failed to find user", "OA002")
 	ErrDiscordTokenExchange     = NewBusinessError(http.StatusUnauthorized, "failed to exchange token", "OA003")
+	ErrDiscordLinkRequired      = NewBusinessError(http.StatusForbidden, "discord account linking is required", "OA004")
 )
